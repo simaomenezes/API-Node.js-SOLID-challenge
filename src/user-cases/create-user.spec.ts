b58f4042b6bcd9da70d86fd734f60c16a51ec58f
Simão Neto <simaomenezesneto@gmail.com>
2023-11-14 12:55:55 +0000

    feat: Create test to verify same email from user

--- src/user-cases/create-user.spec.ts
+++ src/user-cases/create-user.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-users
 import { expect, describe, it, beforeEach } from 'vitest'
 import { CreateUserCase } from './create-user'
 import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 let usersRepository: InMemoryUserRepository
 let sut: CreateUserCase
@@ -13,7 +14,7 @@ describe('Register Use Case', () => {
         sut = new CreateUserCase(usersRepository)
     })
 
-    it('Should be able to create', async() => {
+    it('Should be able to registration', async() => {
 
         const { user } = await sut.execute({
             name: 'Fulano',
@@ -37,6 +38,23 @@ describe('Register Use Case', () => {
         )
 
         expect(isPasswordCorrectLyHashed).toEqual(true)
+    })
+
+    it('should not be able to register with same email twice', async() => {
+        const email = 'fulano@mail.com'
+        await sut.execute({ 
+            name: 'Fulano', 
+            email, 
+            password: '123456' 
+        })
+
+        await expect(() => 
+            sut.execute({ 
+                name: 'Fulano', 
+                email, 
+                password: '123456' 
+            }),
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
     })
     
 })
